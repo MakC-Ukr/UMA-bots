13f9c9aaa2a3fe2bab58a5fb3074331ef8b515ce
maksimjeet20566 <98419737+MakC-Ukr@users.noreply.github.com>
2022-08-11 14:25:52 +0530

    test: added basic test for bot 1

--- src/agent.spec.ts
+++ src/agent.spec.ts
@@ -1,74 +1,69 @@
-// import {
-//   FindingType,
-//   FindingSeverity,
-//   Finding,
-//   HandleTransaction,
-//   createTransactionEvent,
-//   ethers,
-// } from "forta-agent";
-// import agent, {
-//   ERC20_TRANSFER_EVENT,
-//   TETHER_ADDRESS,
-//   TETHER_DECIMALS,
-// } from "./agent";
+import {
+  FindingType,
+  FindingSeverity,
+  Finding,
+  HandleTransaction,
+  createTransactionEvent,
+  ethers,
+} from "forta-agent";
+import { ADD_LIQUIDITY_EVENT, HUBPOOL_ADDRESS } from "./constants";
+import agent from "./agent";
 
-// describe("high tether transfer agent", () => {
-//   let handleTransaction: HandleTransaction;
-//   const mockTxEvent = createTransactionEvent({} as any);
+describe("high tether transfer agent", () => {
+  let handleTransaction: HandleTransaction;
+  const mockTxEvent = createTransactionEvent({} as any);
 
-//   beforeAll(() => {
-//     handleTransaction = agent.handleTransaction;
-//   });
+  beforeAll(() => {
+    handleTransaction = agent.handleTransaction;
+  });
 
-//   describe("handleTransaction", () => {
-//     it("returns empty findings if there are no Tether transfers", async () => {
-//       mockTxEvent.filterLog = jest.fn().mockReturnValue([]);
+  describe("handleTransaction", () => {
+    it("returns empty findings if there is no deposit added", async () => {
+      mockTxEvent.filterLog = jest.fn().mockReturnValue([]);
 
-//       const findings = await handleTransaction(mockTxEvent);
+      const findings = await handleTransaction(mockTxEvent);
 
-//       expect(findings).toStrictEqual([]);
-//       expect(mockTxEvent.filterLog).toHaveBeenCalledTimes(1);
-//       expect(mockTxEvent.filterLog).toHaveBeenCalledWith(
-//         ERC20_TRANSFER_EVENT,
-//         TETHER_ADDRESS
-//       );
-//     });
+      expect(findings).toStrictEqual([]);
+      expect(mockTxEvent.filterLog).toHaveBeenCalledTimes(1);
+      expect(mockTxEvent.filterLog).toHaveBeenCalledWith(
+        ADD_LIQUIDITY_EVENT,
+        HUBPOOL_ADDRESS
+      );
+    });
 
-//     it("returns a finding if there is a Tether transfer over 10,000", async () => {
-//       const mockTetherTransferEvent = {
-//         args: {
-//           from: "0xabc",
-//           to: "0xdef",
-//           value: ethers.BigNumber.from("20000000000"), //20k with 6 decimals
-//         },
-//       };
-//       mockTxEvent.filterLog = jest
-//         .fn()
-//         .mockReturnValue([mockTetherTransferEvent]);
+    it("returns a finding if there is liquidity added from the relevat address", async () => {
+      const mockLiquidityAddedEvent = {
+        args: {
+          l1Token: "0xdef",
+          amount: "0x123",
+          lpTokensMinted: "0x420",
+          liquidityProvider: "0xdef",
+        },
+      };
+      mockTxEvent.filterLog = jest
+        .fn()
+        .mockReturnValue([mockLiquidityAddedEvent]);
 
-//       const findings = await handleTransaction(mockTxEvent);
-
-//       const normalizedValue = mockTetherTransferEvent.args.value.div(
-//         10 ** TETHER_DECIMALS
-//       );
-//       expect(findings).toStrictEqual([
-//         Finding.fromObject({
-//           name: "High Tether Transfer",
-//           description: `High amount of USDT transferred: ${normalizedValue}`,
-//           alertId: "FORTA-1",
-//           severity: FindingSeverity.Low,
-//           type: FindingType.Info,
-//           metadata: {
-//             to: mockTetherTransferEvent.args.to,
-//             from: mockTetherTransferEvent.args.from,
-//           },
-//         }),
-//       ]);
-//       expect(mockTxEvent.filterLog).toHaveBeenCalledTimes(1);
-//       expect(mockTxEvent.filterLog).toHaveBeenCalledWith(
-//         ERC20_TRANSFER_EVENT,
-//         TETHER_ADDRESS
-//       );
-//     });
-//   });
-// });
+      const findings = await handleTransaction(mockTxEvent);
+      expect(findings).toStrictEqual([
+        Finding.fromObject({
+          name: "Across v2 Liquidity Added",
+          description: `Liquidity added into the HubPool`,
+          alertId: "LIQ-DEPO",
+          severity: FindingSeverity.Low,
+          type: FindingType.Info,
+          protocol: "Across v2",
+          metadata: {
+            l1Token: "0xdef",
+            amount: "0x123",
+          },
+        }),
+      ]);
+      expect(mockTxEvent.filterLog).toHaveBeenCalledTimes(1);
+      expect(mockTxEvent.filterLog).toHaveBeenCalledWith(
+        ADD_LIQUIDITY_EVENT,
+        HUBPOOL_ADDRESS
+      );
+    });
+  });
+});
